23b4368238091ef5b30904858ce857e4c9b320e1
Peter Siemens <peterjsiemens@gmail.com>
2016-09-02 12:21:13 -0700

    Initial version

---
+++ main.js
@@ -0,0 +1,90 @@
+var KEY_VIEWED = 'goodnews_viewed';
+var KEY_LINKS  = 'goodnews_links';
+var KEY_TIMESTAMP = 'goodnews_timestamp';
+
+function getNow() {
+  return Math.floor(Date.now() / 1000);
+}
+
+function isStale(timestamp) {
+  return (getNow() - timestamp) > 3600; // Timestamp is older than 1 hour
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+function main() {
+
+  var links = JSON.parse(localStorage.getItem(KEY_LINKS)),
+      timestamp = parseInt(localStorage.getItem(KEY_TIMESTAMP), 10) || 0;
+
+  if ( !isStale(timestamp) && links) {
+    processLinks(links);
+  } else {
+    fetchLinks(processLinks);
+  }
+
+}
+
+function fetchLinks(callback) {
+  return reddit.hot('goodnews').limit(25).fetch(function(res) {
+
+    var links = res.data.children || [];
+
+    links = links.map(function(o) {
+      return {
+        id: o.data.id,
+        title: o.data.title,
+        url: o.data.url,
+        domain: o.data.domain
+      };
+    });
+
+    localStorage.setItem(KEY_LINKS, JSON.stringify(links));
+    localStorage.setItem(KEY_TIMESTAMP, getNow());
+
+    return callback(links);
+  });
+}
+
+function processLinks(links) {
+  var items = JSON.parse(localStorage.getItem(KEY_VIEWED)) || [];
+
+  if (items.length >= 25) {
+    items = [];
+  }
+
+  for (var i = 0; i < links.length; i++) {
+    if (items.indexOf(links[i].id) === -1) {
+      items.push(links[i].id);
+      localStorage.setItem(KEY_VIEWED, JSON.stringify(items));
+      return renderLink(links[i]);
+    }
+  }
+}
+
+function renderLink(link) {
+  $('body').addClass('grad-' + randomNumber(1, 5));
+
+  var $title = $('<a class="link-title">');
+
+  $title.text(link.title);
+  $title.attr('href', link.url);
+
+  var $source = $('<div class="link-source">');
+
+  $source.text(link.domain);
+
+  $('.container').append($title);
+  $('.container').append($source);
+}
+
+main();
